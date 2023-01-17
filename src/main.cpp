70f0af6da6d52387aecece55c519c6e7569a8703
Hunter Rafuse <hunter_r_mazza@protonmail.com>
2023-01-17 18:50:31 +0200

    Add base structure, add base dirs, add base code coverage in cmake, add third_party lib dir

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Test compiled" << std::endl;
+    return 0;
+}
---
+++ utils/generate_benchmark.py
@@ -0,0 +1,6 @@
+"""
+This script generates a 3D benchmark dataset for the specified GPU. Yes, in most cases you would
+use a blanketed benchmark test for each GPU, but this is a good way to test the performance of the GPU due
+to it being specified for said specific GPU. This will also expand to utilizing tests
+for pipelines in regards to CPUs->GPUs->DPUs. Some benchmarks can and will be similar.
+"""
